2126e0cc14a852654eb9a7a2a08b2a2d2cd5ff00
ayoubajboun10-tech <ayoub.ajboun10@gmail.com>
2026-08-26 23:51:05 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
 window.SUPABASE_URL = "https://dgepzjyplnovlissexmy.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_IITQqIX_7vM1cZhKIZiQDQ_c9rRZzbQ";
+
+window.SUPABASE_ANON_KEY =
+  "sb_publishable_IITQqIX_7vM1cZhKIZiQDQ_c9rRZzbQ";
